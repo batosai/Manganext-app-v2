018dd563063e98086649c6af6c2437c1a90d3e7d
Jeremy <jeremy@opsone.net>
2015-08-20 22:57:55 +0200

    Remove admob

--- www/js/config.js
+++ www/js/config.js
@@ -11,8 +11,6 @@ var notificationHasPermission = false;
 
 app.constant('Config', {
     url: ENV == 'dev' ? DEV : PROD,
-    adId: 'ca-app-pub-7709867768399573/6607258151',
-    adIdBottom: 'ca-app-pub-7709867768399573/2885285351',
     appId: '998907e6',
     apiKey: '3b459640b965dd25d93d0007415d83aaaea2b4749cdc29ac',
     defaultLanguage: 'fr',
